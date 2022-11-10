5b14a178cf92da01f2908aee94140879ba087ce7
Julio J. Bascunan <jbascunan@arica.ts>
2022-11-10 20:12:31 -0300

    revisando con el profe el codigo me falta el Update y el borrar

--- BD/conexion.py
+++ BD/conexion.py
@@ -1,12 +1,60 @@
 import mysql.connector
+from mysql.connector import Error
 
 
-try:
-    conexion = mysql.connector.connect(user="jbascunan",password="Isabellabascu1409",host="localhost",database="pruebapy",port="3306")
+class DAO():
 
-    print("conexion exitosa")
+    def __init__(self):
+        try:
+            self.conexion = mysql.connector.connect(
+                host='localhost',
+                port=3306,
+                user='jbascunan',
+                password='Isabellabascu1409',
+                db='pruebapy'
+            )
+        except Error as ex:
+            print("Error al intentar la conexión: {0}".format(ex))
 
-except:
-    print("error de conexion")
+    def listarLibros(self):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                cursor.execute("SELECT * FROM libros ORDER BY nombre ASC")
+                resultados = cursor.fetchall()
+                return resultados
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))
 
+    def registrarLibro(self, curso):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sql = "INSERT INTO libros (codigo, nombre, creditos) VALUES ('{0}', '{1}', {2})"
+                cursor.execute(sql.format(curso[0], curso[1], curso[2]))
+                self.conexion.commit()
+                print("¡Libro registrado!\n")
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))
 
+    def actualizarLibro(self, curso):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sql = "UPDATE libros SET nombre = '{0}', creditos = {1} WHERE codigo = '{2}'"
+                cursor.execute(sql.format(curso[1], curso[2], curso[0]))
+                self.conexion.commit()
+                print("¡Libro actualizado!\n")
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))
+
+    def eliminarLibro(self, codigoLibroEliminar):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                sql = "DELETE FROM libros WHERE codigo = '{0}'"
+                cursor.execute(sql.format(codigoLibroEliminar))
+                self.conexion.commit()
+                print("¡Libro eliminado!\n")
+            except Error as ex:
+                print("Error al intentar la conexión: {0}".format(ex))
--- main.py
+++ main.py
@@ -1,11 +1,78 @@
-from BD.conexion import *
+from BD.conexion import DAO
+import funciones
 
-def menu():
-    print("""
-    =================MENU PRINCIPAL===================
-    |   1.- Listar Libros       2.- Registrar Libros |
-    |   3.- Actualizar Libro    4.- Eliminar Libro   |
-    |   5.- Salir                                    |
-    ==================================================
-    """)
-    
+
+def menuPrincipal():
+    continuar = True
+    while(continuar):
+        opcionCorrecta = False
+        while(not opcionCorrecta):
+            print("==================== MENÚ PRINCIPAL ====================")
+            print("1.- Listar Libros")
+            print("2.- Registrar Libro")
+            print("3.- Actualizar Libro")
+            print("4.- Eliminar Libro")
+            print("5.- Salir")
+            print("========================================================")
+            opcion = int(input("Seleccione una opción: "))
+
+            if opcion < 1 or opcion > 5:
+                print("Opción incorrecta, ingrese nuevamente...")
+            elif opcion == 5:
+                continuar = False
+                print("¡Gracias por usar este sistema!")
+                break
+            else:
+                opcionCorrecta = True
+                ejecutarOpcion(opcion)
+
+
+def ejecutarOpcion(opcion):
+    dao = DAO()
+
+    if opcion == 1:
+        try:
+            libros = dao.listarLibros()
+            if len(libros) > 0:
+                funciones.listarLibros(libros)
+            else:
+                print("No se encontraron libros...")
+        except:
+            print("Ocurrió un error...")
+    elif opcion == 2:
+        curso = funciones.pedirDatosRegistro()
+        try:
+            dao.registrarLibro(curso)
+        except:
+            print("Ocurrió un error...")
+    elif opcion == 3:
+        try:
+            libros = dao.listarLibros()
+            if len(libros) > 0:
+                curso = funciones.pedirDatosActualizacion(libros)
+                if curso:
+                    dao.actualizarLibro(curso)
+                else:
+                    print("Código de libro a actualizar no encontrado...\n")
+            else:
+                print("No se encontraron libros...")
+        except:
+            print("Ocurrió un error...")
+    elif opcion == 4:
+        try:
+            libros = dao.listarLibros()
+            if len(libros) > 0:
+                codigoEliminar = funciones.pedirDatosEliminacion(libros)
+                if not(codigoEliminar == ""):
+                    dao.eliminarLibro(codigoEliminar)
+                else:
+                    print("Código de libro no encontrado...\n")
+            else:
+                print("No se encontraron libros...")
+        except:
+            print("Ocurrió un error...")
+    else:
+        print("Opción no válida...")
+
+
+menuPrincipal()
